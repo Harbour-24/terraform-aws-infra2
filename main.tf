21c2ad6ab9f81ee5d6c4f18e4e57c248ea71b8ea
岩間大晃 <iwamadaiakira@iwamadaiakiranoMacBook-Air.local>
2025-09-29 11:41:38 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+	required_providers {
+	   aws = {
+	       source = "hashicorp/aws"
+	       version = "~> 5.41.0"
+	   }
+	}
+	cloud {
+	  organization = "aws-infra2026"
+
+	  workspaces {
+	    name = "aws-infra"
+	  }
+	}
+}
+
+provider "aws" {
+	region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0eba6c58b7918d3a1"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "TestInstance"
+  }
+}
